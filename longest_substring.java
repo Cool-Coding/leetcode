21ae085eb170dfae73fb195430b7a3b0bae46842
Cool-Coding <riguang_2007@163.com>
2018-06-02 21:28:42 +0800

    Update longest_substring.java
    暴力法非常简单。但它太慢了。那么我们该如何优化它呢？
    
    在暴力法中，我们会反复检查一个子字符串是否含有有重复的字符，但这是没有必要的。如果从索引 ii 到 j - 1j−1 之间的子字符串 s_{ij}s
    ​ij
    ​​  已经被检查为没有重复字符。我们只需要检查 s[j]s[j] 对应的字符是否已经存在于子字符串 s_{ij}s
    ​ij
    ​​ 中。
    
    要检查一个字符是否已经在子字符串中，我们可以检查整个子字符串，这将产生一个复杂度为 O(n^2)O(n
    ​2
    ​​ ) 的算法，但我们可以做得更好。
    
    通过使用 HashSet 作为滑动窗口，我们可以用 O(1)O(1) 的时间来完成对字符是否在当前的子字符串中的检查。
    
    滑动窗口是数组/字符串问题中常用的抽象概念。 窗口通常是在数组/字符串中由开始和结束索引定义的一系列元素的集合，即 [i, j)[i,j)（左闭，右开）。而滑动窗口是可以将两个边界向某一方向“滑动”的窗口。例如，我们将 [i, j)[i,j) 向右滑动 11 个元素，则它将变为 [i+1, j+1)[i+1,j+1)（左闭，右开）。
    
    回到我们的问题，我们使用 HashSet 将字符存储在当前窗口 [i, j)[i,j)（最初 j = ij=i）中。 然后我们向右侧滑动索引 jj，如果它不在 HashSet 中，我们会继续滑动 jj。直到 s[j] 已经存在于 HashSet 中。此时，我们找到的没有重复字符的最长子字符串将会以索引 ii 开头。如果我们对所有的 ii 这样做，就可以得到答案。

--- longest_substring.java
+++ longest_substring.java
@@ -1,42 +1,18 @@
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int len=s.length();//字符串长度
-        int start;//相应长度对应的起始标志
-        int end;//相应长度对应的结束标志
-        int index;//长度内字符指针
-        boolean replicated=false;
-
-        //字符集合
-        Set<Character> charSet=new HashSet<Character>();
-        for(int i=len;i>0;i--){
-            start=0;//取每一个长度时，都从0位置开始
-            while(true){
-                end = start + i;//结束标志
-                if(end > len){
-                    break;
-                }
-
-                index = start;
-                charSet.clear();
-                replicated=false;
-                while(index < end ){
-                    char c=s.charAt(index);
-                    if(charSet.contains(c)){
-                        replicated=true;
-                        break;
-                    }else{
-                        charSet.add(c);
-                    }
-                    index++;
-                }
-                //集合中所有字符不相同，则说明长到了
-                if(!replicated){
-                    return i;
-                }
-
-                start++;//起始标志加1
+        int n = s.length();
+        Set<Character> set = new HashSet<Character>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
             }
         }
-        return 0;
+        return ans;
     }
 }
